b9702b22f929ada5ca2d309d5de9377f1a35371b
vinayakgarole <vinayakgarole10@gmail.com>
2022-09-24 00:09:11 +0530

    added: Regex user registration

--- src/com/bridgelabz/UserValid.java
+++ src/com/bridgelabz/UserValid.java
@@ -1,15 +1,25 @@
 package com.bridgelabz;
 import java.util.Scanner;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 public class UserValid {
     public static void main(String[] args) {
 
-    Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         // Method To Check First Name With Regex Start With Cap And 3 Min. Character
         System.out.println("Enter the first Name: ");
         String firstName = scan.nextLine();
+        System.out.println("Enter the last Name :");
+        String lastName = scan.nextLine();
 
         if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        // Method To Check Last Name With Regex Start With Cap And 3 Min. Character
+        if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
             System.out.println("True");
         } else {
             System.out.println("False");
